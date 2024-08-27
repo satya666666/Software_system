95d84099d9c67cbda7a1a5b9bb8d6bc7999174cf
satya666666 <rrohitgupta582@gmail.com>
2024-08-27 18:06:13 +0530

    2nd commit

---
+++ 8.c
@@ -0,0 +1,23 @@
+// Name - Satyam Gupta, Roll no.- MT2024134
+// Question: Write a program to open a file in read only mode, read line by line and display each line as it is read. Close the file when end of file is reached.
+
+#include <fcntl.h>       // for open the file
+#include <unistd.h>      // for read, write and close operations
+#include <stdio.h>
+int main() {
+int fd = open("Example.txt", O_RDONLY);   // open the Exmaple.txt in read mode only
+if (fd == -1) {
+perror("open");
+return 1;
+}
+char buffer[256];
+size_t bytesRead;                          // to store the size of block of data in bytes
+while ((bytesRead = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
+buffer[bytesRead] = '\0';                 // add null character to the end of the string
+printf("%s", buffer);
+}
+close(fd);
+return 0;
+}
+
+// Output:
