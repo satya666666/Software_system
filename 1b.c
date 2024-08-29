f81a5a211fdb9013b1fe3ef84b70f9e9550c1122
satya666666 <42898509+satya666666@users.noreply.github.com>
2024-08-29 19:20:17 +0530

    Updated

--- 1b.c
+++ 1b.c
@@ -10,3 +10,16 @@ perror("link");                               // used for tells about the error 
 }
 return 0;
 }
+
+// Output:
+
+// satyam@satyam-X542UQ:~/Desktop/Software_system-File$ cc 1b.c
+// satyam@satyam-X542UQ:~/Desktop/Software_system-File$ ./a.out
+// satyam@satyam-X542UQ:~/Desktop/Software_system-File$ ls
+// 1a.c  1b.c  1c.c  1shell.sh  2.c  2shell.sh  3.c  4.c  5.c  a.out  symlink1  symlink2
+// satyam@satyam-X542UQ:~/Desktop/Software_system-File$ cat symlink2
+// # This is my first Program of Shell Scripting language i.e., used for create softlink, hardlink and Fifo by shell command.
+
+// #!/bin/bash
+
+// echo "This is the basic program for hardlink to make by system call. 
