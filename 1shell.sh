ba357cc32703202170b44e9b5724538bc93c40bc
satya666666 <42898509+satya666666@users.noreply.github.com>
2024-08-30 17:50:36 +0530

    Delete 1shell.sh

--- 1shell.sh
+++
@@ -1,5 +0,0 @@
-# This is my first Program of Shell Scripting language i.e., used for create softlink, hardlink and Fifo by shell command.
-
-#!/bin/bash
-
-echo "Hello world, I am learning shell scripting. "
